878a2c511143c33121e1ce156639b87d7cf5dab9
Samueldux <samueloliveira1914@hotmail.com>
2025-01-05 11:25:04 -0300

    fix: aceitar valores nulos no campo ProfilePicture

--- Back-End/UserManagement.Domain/Entities/User.cs
+++ Back-End/UserManagement.Domain/Entities/User.cs
@@ -23,15 +23,38 @@ namespace UserManagement.Domain.Entities
         [StringLength(10, ErrorMessage = "Gender cannot be longer than 10 characters.")]
         public string Gender { get; set; }
 
+
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        [StringLength(15, ErrorMessage = "Phone cannot be longer than 15 characters.")]
+        public string Phone { get; set; }
+
+        [Phone(ErrorMessage = "Invalid cell phone number format.")]
+        [StringLength(15, ErrorMessage = "Cell phone cannot be longer than 15 characters.")]
+        public string Cell { get; set; }
+
         [Required(ErrorMessage = "City is required.")]
         [StringLength(50, ErrorMessage = "City cannot be longer than 50 characters.")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Street Name is required.")]
+        [StringLength(100, ErrorMessage = "Street Name cannot be longer than 100 characters.")]
+        public string StreetName { get; set; }
+
+        [Required(ErrorMessage = "Street Number is required.")]
+      
+        public int StreetNumber { get; set; }
+
+        [Required(ErrorMessage = "State is required.")]
+        [StringLength(50, ErrorMessage = "State cannot be longer than 50 characters.")]
+        public string State { get; set; }
 
         [Required(ErrorMessage = "Country is required.")]
         [StringLength(50, ErrorMessage = "Country cannot be longer than 50 characters.")]
         public string Country { get; set; }
 
-        [Url(ErrorMessage = "Invalid URL format.")]
-        public string ProfilePicture { get; set; }
+        [Required(ErrorMessage = "Postal Code is required.")]
+      
+        public int PostCode { get; set; }
+        public string? ProfilePicture { get; set; }
     }
 }
